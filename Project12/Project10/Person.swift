29101c0c18a10705de06292c9b037740aef38667
yurongde <359067441@qq.com>
2016-01-27 11:26:49 +0800

    finish project 12

---
+++ Project12/Project10/Person.swift
@@ -0,0 +1,28 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by yurongde on 16/1/22.
+//  Copyright © 2016年 yurongde. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject,NSCoding {
+    var name: String
+    var image: String
+    
+    init(name: String,image: String) {
+        self.name = name
+        self.image = image
+    }
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObjectForKey("name") as! String
+        image = aDecoder.decodeObjectForKey("image") as! String
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+     aCoder.encodeObject(name, forKey: "name")
+        aCoder.encodeObject(image, forKey: "image")
+    }
+}
